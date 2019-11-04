8cd0000585eb98eb09fc3940277ac64c341376d4
Stephanie A <stephanie.m.aurelio@gmail.com>
2019-11-04 10:39:26 -0800

    Updated UV index AJAX call and five day forecast DOM manipulation

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function storeSearches() {
 
 $("#add-city").on("click", function(event) {
     event.preventDefault();
+
     var city = $("#city-input").val();
 
     // Add city to array of city names
@@ -38,6 +39,7 @@ function currentWeather() {
         url: queryURL,
         method: "GET"
         }).then(function(response) {
+        console.log(response);
         var weatherMain = $("<div class='current-weather'>");
 
         var cityTitle = response.name;
@@ -70,7 +72,29 @@ function currentWeather() {
 
         weatherMain.append(pThree);
 
-        // var uvIndex = 
+        // New AJAX call for the UV Index
+        function uvIndex() {
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+
+            var queryURLUv = "http://api.openweathermap.org/data/2.5/uvi?appid=6a4885bca485162d035533a77b0473df&lat=" + lat + "&lon=" + lon;
+
+            $.ajax({
+                url: queryURLUv,
+                method: "GET"
+            }).then(function(response){
+                console.log("This is the UV" + response);
+            })
+        }
+
+        // DOM manipulation to show UV Index
+        uvIndex();
+
+        var uv = response.value;
+        var pFour = $("<p>").text("UV Index: " + uv);
+
+        weatherMain.append(pFour);
+
 
         $("#current-weather-display").append(weatherMain);
     });
@@ -79,6 +103,7 @@ function currentWeather() {
 // AJAX call using search term cityName for five-day forecast
 
 function fiveDayForecast() {
+
     var city = $(this).attr("data-name");
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&units=imperial&APPID=6a4885bca485162d035533a77b0473df";
 
@@ -88,7 +113,50 @@ function fiveDayForecast() {
     }).then(function(response) {
         console.log(response);
 // DOM manipulation to display search results for five-day forecast
+        var dailyTemps = [];
+        for(var i = 0; i <= 4; i++){
+            console.log(response.list[i].main.temp);
+            dailyTemps.push(response.list[i].main.temp);
+            console.log(dailyTemps);
+        }
+
+        var dayOneWeatherDisplay = $("<div>");
+
+        var dayOneTemp = dailyTemps[0];
+        var pFive = $("<p>").text("Temp: " + dayOneTemp);
+        dayOneWeatherDisplay.append(pFive);
+        $("#day-1").append(dayOneWeatherDisplay);
+
+        var dayTwoWeatherDisplay = $("<div>");
+
+        var dayTwoTemp = dailyTemps[1];
+        var pSix = $("<p>").text("Temp: " + dayTwoTemp);
+        dayTwoWeatherDisplay.append(pSix);
+        $("#day-2").append(dayTwoWeatherDisplay);
+
+        var dayThreeWeatherDisplay = $("<div>");
+
+        var dayThreeTemp = dailyTemps[2];
+        var pSeven = $("<p>").text("Temp: " + dayThreeTemp);
+        dayThreeWeatherDisplay.append(pSeven);
+        $("#day-3").append(dayThreeWeatherDisplay);
+
+        var dayFourWeatherDisplay = $("<div>");
+
+        var dayFourTemp = dailyTemps[3];
+        var pEight = $("<p>").text("Temp: " + dayFourTemp);
+        dayFourWeatherDisplay.append(pEight);
+        $("#day-4").append(dayFourWeatherDisplay);
+
+        var dayFiveWeatherDisplay = $("<div>");
+
+        var dayFiveTemp = dailyTemps[4];
+        var pNine = $("<p>").text("Temp: " + dayFiveTemp);
+        dayFiveWeatherDisplay.append(pNine);
+        $("#day-5").append(dayFiveWeatherDisplay);
     });
+
+
 }
 
 
